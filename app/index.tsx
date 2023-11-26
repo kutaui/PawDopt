1f8f3238a01b59b557369a7745002c6587995654
kutaui <kutaybekleric@gmail.com>
2023-11-26 12:16:34 +0300

    finish landing page

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,33 @@
 import { View, Text, TouchableWithoutFeedback, Image } from 'react-native';
 import { Link } from 'expo-router';
 import { lineHeight } from 'nativewind/dist/tailwind/native/line-height';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 const App = () => {
     return (
-        <View className="bg-white w-full h-full flex items-center ">
+        <View className="bg-white w-full h-[80%] flex items-center justify-between mt-4">
             <Image
                 className="w-72 h-72"
                 source={require('../assets/Untitled.png')}
                 resizeMode="contain"
             />
-            <View className="w-[85%] pt-20">
-            <Text style={{lineHeight:70}} className="text-5xl font-semibold text-black/75 ">Don't Shop, Adopt.</Text>
-            <View className="w-[60%] border-b-2 my-4 border-zinc-400 "/>
-            <Text className="text-3xl text-black/75">Ready to make a new friend?
-            </Text>
+            <View className="w-[85%]">
+                <Text style={{lineHeight: 70}} className="text-5xl font-semibold text-black/75 ">Don't Shop,
+                    Adopt.</Text>
+                <View className="w-[60%] border-b-2 my-4 border-zinc-400 "/>
+                <Text className="text-2xl text-black/75">Ready to make a new friend?
+                </Text>
             </View>
-            {/*<Link href="/login" asChild>
-                <TouchableWithoutFeedback><Text className="w-20 bg-red-700"> Login</Text></TouchableWithoutFeedback>
-            </Link>*/}
+            <View className="w-52 h-28 bg-violet-600 rounded-tl-full rounded-tr-full absolute -bottom-40 -right-24">
+                <View className="absolute left-12 bottom-8">
+                    <Link href="/login" asChild>
+
+                        <AntDesign name="arrowright" size={40} color="white"/>
+                    </Link>
+
+                </View>
+            </View>
+
         </View>
     );
 };
